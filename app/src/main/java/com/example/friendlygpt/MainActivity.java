695c76bc3fc5bf3bd9c36952395aef83b6bd3d44
Heena-dotcom <heenabajariya94@gmail.com>
2023-03-30 09:54:38 -0400

    Initial commit

--- app/src/main/java/com/example/friendlygpt/MainActivity.java
+++ app/src/main/java/com/example/friendlygpt/MainActivity.java
@@ -1,16 +1,32 @@
 package com.example.friendlygpt;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
@@ -18,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
     EditText messageEditText;
     ImageButton sendButton;
     List<Message> messageList;
+    MessageAdaptor messageAdaptor;
+    public static final MediaType JSON
+            = MediaType.get("application/json; charset=utf-8");
+
+    OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,10 +52,78 @@ public class MainActivity extends AppCompatActivity {
         messageEditText = findViewById(R.id.etMessage);
         sendButton = findViewById(R.id.btnSend);
 
+        messageAdaptor = new MessageAdaptor(messageList);
+        recyclerView.setAdapter(messageAdaptor);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setStackFromEnd(true);
+        recyclerView.setLayoutManager(llm);
+
         sendButton.setOnClickListener((v)->{
             String question = messageEditText.getText().toString().trim();
-
+            addToChat(question,Message.SENT_BY_ME);
+            callAPI(question);
+            messageEditText.setText("");
+            welcomeTextView.setVisibility(View.GONE);
         });
+    }
 
+    void addToChat(String message, String sentBy){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                messageList.add(new Message(message,sentBy));
+                messageAdaptor.notifyDataSetChanged();
+                recyclerView.smoothScrollToPosition(messageAdaptor.getItemCount());
+            }
+        });
+    }
+
+    void addResponse(String response){
+        messageList.remove(messageList.size()-1);
+        addToChat(response,Message.SENT_BY_BOT);
+    }
+    void callAPI(String question){
+        messageList.add(new Message("Typing... ",Message.SENT_BY_BOT));
+
+
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("model","text-davinci-003");
+            jsonObject.put("prompt",question);
+            jsonObject.put("max_tokens",4000);
+            jsonObject.put("temperature",0);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        RequestBody requestBody = RequestBody.create(jsonObject.toString(),JSON);
+        Request request = new Request.Builder()
+                .url("https://api.openai.com/v1/completions")
+                .header("Authorization","Bearer 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")
+                .post(requestBody)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                addResponse("Failed to load response due to "+ e.getMessage());
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()){
+                    JSONObject json = null;
+                    try {
+                        json = new JSONObject(response.body().string());
+                        JSONArray jsonArray = json.getJSONArray("choices");
+                        String result = jsonArray.getJSONObject(0).getString("text");
+                        addResponse(result.trim());
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                }else {
+                    addResponse("Failed to load response due to "+ response.body().string());
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/friendlygpt/MessageAdaptor.java
+++ app/src/main/java/com/example/friendlygpt/MessageAdaptor.java
@@ -1,13 +1,48 @@
 package com.example.friendlygpt;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MessageAdaptor {
+import java.util.List;
+
+public class MessageAdaptor extends RecyclerView.Adapter<MessageAdaptor.MyViewHolder>{
+    List<Message> messageList;
+    public MessageAdaptor(List<Message> messageList) {
+        this.messageList=messageList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View chatView = LayoutInflater.from(parent.getContext()).inflate(R.layout.chat_item,null);
+        MyViewHolder myViewHolder = new MyViewHolder(chatView);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Message message = messageList.get(position);
+        if (message.getSentBy().equals(Message.SENT_BY_ME)){
+            holder.loutLeftChat.setVisibility(View.GONE);
+            holder.loutRigthChat.setVisibility(View.VISIBLE);
+            holder.rightTextView.setText(message.getMessage());
+        }else{
+            holder.loutRigthChat.setVisibility(View.GONE);
+            holder.loutLeftChat.setVisibility(View.VISIBLE);
+            holder.leftTextView.setText(message.getMessage());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return messageList.size();
+    }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
     LinearLayout loutLeftChat, loutRigthChat;
@@ -16,6 +51,10 @@ public class MessageAdaptor {
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             loutLeftChat = itemView.findViewById(R.id.loutLeftChat);
+            loutRigthChat = itemView.findViewById(R.id.loutRightChat);
+            leftTextView = itemView.findViewById(R.id.tvLeftChat);
+            rightTextView = itemView.findViewById(R.id.tvRightChat);
+
         }
     }
 
